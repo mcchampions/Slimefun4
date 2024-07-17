41c46a1a9991d454e561c113bcbf25bcc41596f8
mcchampions <1309635304@qq.com>
2024-07-17 18:58:20 +0800

    fix(script): 修复了错误长度的sha问题

--- .github/updateVersion.js
+++ .github/updateVersion.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 let xml = fs.readFileSync('pom.xml', {
     encoding: "utf-8",
 });
-
+const sha = process.env.GITHUB_SHA.substring(0, 7);
 xml = xml.replace("<version>UNOFFICIAL</version>",
-    "<version>" + process.env.GITHUB_SHA + "</version>");
+    "<version>" + sha + "</version>");
 fs.writeFileSync('pom.xml', xml)
