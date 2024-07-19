6e46a7ef6d2c9ff031adc7675d1ebcecb3c87480
mcchampions <1309635304@qq.com>
2024-07-19 21:59:31 +0800

    chore: remove listener

--- src/main/java/city/norain/slimefun4/listener/SlimefunMigrateListener.java
+++ src/main/java/city/norain/slimefun4/listener/SlimefunMigrateListener.java
@@ -5,6 +5,6 @@ import org.bukkit.event.Listener;
 
 public class SlimefunMigrateListener implements Listener {
     public void register(Slimefun plugin) {
-        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+        // plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
 }
