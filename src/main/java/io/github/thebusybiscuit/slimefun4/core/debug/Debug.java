7ca07fa344ab176b53f92e9c4f01acf535f1c5c1
mcchampions <1309635304@qq.com>
2024-08-02 12:18:09 +0800

    Update Debug.java

--- src/main/java/io/github/thebusybiscuit/slimefun4/core/debug/Debug.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/core/debug/Debug.java
@@ -66,38 +66,7 @@ public final class Debug {
      *            The variables to replace, use "{}" in the message and have it replaced with a specified thing
      */
     public static void log(String test, String msg, Object... vars) {}
-
-    /**
-     * Format the message. Replace "{}" with the supplied variable. This is quick and works great.
-     * <p>
-     * <code>
-     * Benchmark                    Mode  Cnt        Score       Error  Units
-     * MyBenchmark.loopAllChars    thrpt    5  2336518.563 ± 24129.488  ops/s
-     * MyBenchmark.whileFindChars  thrpt    5  3319022.018 ± 45663.898  ops/s
-     * </code>
-     *
-     * @param msg
-     *            The message to send. For variables, you can pass "{}"
-     * @param vars
-     *            A varargs of the variables you wish to use
-     *
-     * @return The resulting String
-     */
-    private static String formatMessage(String msg, Object... vars) {
-        int i = 0;
-        int idx = 0;
-
-        // Find an opening curly brace `{` and validate the next char is a closing one `}`
-        while ((i = msg.indexOf('{', i)) != -1 && msg.charAt(i + 1) == '}') {
-            // Substring up to the opening brace `{`, add the variable for this and add the rest of the
-            // message
-            msg = msg.substring(0, i) + vars[idx] + msg.substring(i + 2);
-            i += String.valueOf(vars[idx++]).length();
-        }
-
-        return msg;
-    }
-
+    
     /**
      * Set the current test case for this server.
      * This will enable debug logging for this specific case which can be helpful by Slimefun or addon developers.
