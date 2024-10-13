3352942f1a2ddf0dfc07cd13f68e8e18d2728abe
mcchampions <1309635304@qq.com>
2024-10-13 11:14:57 +0800

    Update GeyserIntegration.java

--- src/main/java/me/qscbm/slimefun4/integrations/GeyserIntegration.java
+++ src/main/java/me/qscbm/slimefun4/integrations/GeyserIntegration.java
@@ -25,44 +25,48 @@ import java.util.List;
  */
 public class GeyserIntegration {
     public void register() {
-        Slimefun.logger().info("开始加载自定义粘液科技Geyser支持");
+        Slimefun.runAsync(() -> {
+            long start = System.nanoTime();
+            Slimefun.logger().info("开始加载自定义粘液科技Geyser支持");
 
-        Config config = new Config(new File("plugins/Geyser-Spigot/custom-skulls.yml"));
-        List<String> l = config.getStringList("skin-hashes");
-        int successCount = 0;
-        int errorCount = 0;
-        for (SlimefunItem slimefunItem : Slimefun.getRegistry().getAllSlimefunItems()) {
-            ItemStack itemStack = slimefunItem.getItem();
-            if (itemStack.getType() != Material.PLAYER_HEAD) {
-                continue;
+            Config config = new Config(new File("plugins/Geyser-Spigot/custom-skulls.yml"));
+            List<String> l = config.getStringList("skin-hashes");
+            int successCount = 0;
+            int errorCount = 0;
+            for (SlimefunItem slimefunItem : Slimefun.getRegistry().getAllSlimefunItems()) {
+                ItemStack itemStack = slimefunItem.getItem();
+                if (itemStack.getType() != Material.PLAYER_HEAD) {
+                    continue;
+                }
+                ItemMeta m = itemStack.getItemMeta();
+                if (!(m instanceof SkullMeta meta)) {
+                    errorCount++;
+                    continue;
+                }
+                String textureCode = NBTUtils.getTexture(meta);
+                if (textureCode == null) {
+                    errorCount++;
+                    continue;
+                }
+                String[] ts = textureCode.split("/");
+                String hash = ts[ts.length - 1];
+                if (!l.contains(hash)) {
+                    l.add(hash);
+                }
+                successCount++;
             }
-            ItemMeta m = itemStack.getItemMeta();
-            if (!(m instanceof SkullMeta meta)) {
-                errorCount++;
-                continue;
+            String[] i = {"player-names", "player-uuids", "player-profiles"};
+            for (String t : i) {
+                if (config.getStringList(t).isEmpty()) {
+                    config.setValue(t, new ArrayList<>());
+                }
             }
-            String textureCode = NBTUtils.getTexture(meta);
-            if (textureCode == null) {
-                errorCount++;
-                continue;
-            }
-            String[] ts = textureCode.split("/");
-            String hash = ts[ts.length - 1];
-            if (!l.contains(hash)) {
-                l.add(hash);
-            }
-            successCount++;
-        }
-        String[] i = {"player-names", "player-uuids", "player-profiles"};
-        for (String t : i) {
-            if (config.getStringList(t).isEmpty()) {
-                config.setValue(t, new ArrayList<>());
-            }
-        }
-        config.setValue("skin-hashes", l);
-        config.save();
-        Slimefun.logger().info("成功加载" + successCount + "个自定义头颅");
-        Slimefun.logger().info("加载失败" + errorCount + "个自定义头颅");
-        Slimefun.logger().warning("完成加载自定义粘液科技Geyser支持,如果不生效请重启服务器");
+            config.setValue("skin-hashes", l);
+            config.save();
+            Slimefun.logger().info("成功加载" + successCount + "个自定义头颅");
+            Slimefun.logger().info("加载失败" + errorCount + "个自定义头颅");
+            Slimefun.logger().info("加载共耗时" + (System.nanoTime() - start) / 1000000 + "ms");
+            Slimefun.logger().warning("完成加载自定义粘液科技Geyser支持,如果不生效请重启服务器");
+        });
     }
 }
