7a41fb2eeff0805eb96f6f336b56964ff6423521
mcchampions <1309635304@qq.com>
2024-10-26 18:02:37 +0800

    chore: 增加药水兼容性支持

--- src/main/java/me/qscbm/slimefun4/helper/ItemHelper.java
+++ src/main/java/me/qscbm/slimefun4/helper/ItemHelper.java
@@ -1,13 +1,15 @@
 package me.qscbm.slimefun4.helper;
 
 import io.github.bakedlibs.dough.config.Config;
-import io.github.bakedlibs.dough.items.CustomItemStack;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
 import io.github.thebusybiscuit.slimefun4.utils.itemstack.ItemStackWrapper;
+import me.qscbm.slimefun4.utils.TextUtils;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.PotionMeta;
 
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 
@@ -15,11 +17,8 @@ public class ItemHelper {
     public static final Map<String, String> ITEM_NAME_MAPPER = new HashMap<>();
 
     public void load() {
+        Slimefun.instance().saveResource("vanilla_items.yml", true);
         Config config = new Config(Slimefun.instance(), "vanilla_items.yml");
-        if (!config.getFile().exists()) {
-            Slimefun.instance().saveResource("vanilla_items.yml", false);
-            config.reload();
-        }
         Set<String> keys = config.getKeys();
         for (String key : keys) {
             ITEM_NAME_MAPPER.put(key.toLowerCase(), config.getString(key));
@@ -28,14 +27,22 @@ public class ItemHelper {
 
     public String getItemName(ItemStack stack) {
         if (stack instanceof SlimefunItemStack sfItem) {
-            return sfItem.getDisplayName();
+            return TextUtils.toPlainText(sfItem.getDisplayName());
         }
         ItemStackWrapper wrapper = ItemStackWrapper.wrap(stack);
         if (wrapper.hasItemMeta()) {
             //noinspection deprecation
-            return wrapper.getItemMeta().getDisplayName();
+            return TextUtils.toPlainText(wrapper.getItemMeta().getDisplayName());
         }
-        String type = wrapper.getType().name();
-        return ITEM_NAME_MAPPER.getOrDefault(type.toLowerCase(), "Unknown Item(id:" + type + ")");
+
+        String type = wrapper.getType().name().toLowerCase(Locale.ROOT);
+        if (type.contains("potion") || type.equals("tipped_arrow")) {
+            //noinspection deprecation
+            String potion = ((PotionMeta) wrapper.getItemMeta()).getBasePotionData().getType().toString().toLowerCase();
+            if (!potion.isEmpty()) {
+                type = potion + "_" + type;
+            }
+        }
+        return ITEM_NAME_MAPPER.getOrDefault(type, "Unknown Item(id:" + type + ")");
     }
 }
