5400e29c4e0f3b9c689f56b179e0d8972953a9b9
mcchampions <1309635304@qq.com>
2024-11-05 18:58:04 +0800

    Update ClearDataCommand.java

--- src/main/java/io/github/thebusybiscuit/slimefun4/core/commands/subcommands/ClearDataCommand.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/core/commands/subcommands/ClearDataCommand.java
@@ -5,6 +5,7 @@ import io.github.thebusybiscuit.slimefun4.api.geo.GEOResource;
 import io.github.thebusybiscuit.slimefun4.core.commands.SlimefunCommand;
 import io.github.thebusybiscuit.slimefun4.core.commands.SubCommand;
 import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,71 +22,74 @@ public class ClearDataCommand extends SubCommand {
 
     @Override
     public void onExecute(CommandSender sender, String[] args) {
-        if (sender.hasPermission("slimefun.command.cleardata") || sender instanceof ConsoleCommandSender) {
-            if (args.length == 4 && args[3].equalsIgnoreCase("confirm")) {
-                List<World> worlds = new ArrayList<>();
-                List<String> clearTypes = new ArrayList<>();
-                String block = Slimefun.getLocalization().getMessage("commands.cleardata.block");
-                String oil = Slimefun.getLocalization().getMessage("commands.cleardata.oil");
-                BlockDataController controller = Slimefun.getDatabaseManager().getBlockDataController();
-                if (args[1].equals("*")) {
-                    worlds.addAll(Bukkit.getWorlds());
-                } else {
-                    World toAdd = Bukkit.getWorld(args[1]);
-                    if (toAdd == null) {
-                        Slimefun.getLocalization().sendMessage(sender, "commands.cleardata.worldNotFound", true);
-                        return;
-                    }
-                }
-
-                if (args[2].equals("*")) {
-                    clearTypes.addAll(ValidClearTypes);
-                } else if (ValidClearTypes.contains(args[2])) {
-                    clearTypes.add(args[2]);
-                }
-
-                for (World world : worlds) {
-                    for (String cleartype : clearTypes) {
-                        if (cleartype.equals("block")) {
-                            controller.removeAllDataInWorldAsync(
-                                    world,
-                                    () -> Slimefun.runSync(() -> Slimefun.getLocalization()
-                                            .sendMessage(sender, "commands.cleardata.success", true, msg -> msg.replace(
-                                                            "{0}", world.getName())
-                                                    .replace("{1}", block))));
-                        } else if (cleartype.equals("oil")) {
-                            GEOResource oilresource = null;
-                            for (GEOResource resource :
-                                    Slimefun.getRegistry().getGEOResources().values()) {
-                                if (resource.getKey()
-                                        .toString()
-                                        .equals(new NamespacedKey(Slimefun.instance(), "oil").toString())) {
-                                    oilresource = resource;
-                                }
-                            }
-                            controller.removeFromAllChunkInWorldAsync(
-                                    world,
-                                    oilresource.getKey().toString().replace(":", "-"),
-                                    () -> Slimefun.runSync(() -> Slimefun.getLocalization()
-                                            .sendMessage(sender, "commands.cleardata.success", true, msg -> msg.replace(
-                                                            "{0}", world.getName())
-                                                    .replace("{1}", oil))));
-                        }
-                    }
-                }
-                return;
-            } else if (args.length == 3) {
-                Slimefun.getLocalization().sendMessage(sender, "commands.cleardata.confirm", true);
-                return;
-            }
+        if (!sender.hasPermission("slimefun.command.cleardata") && !(sender instanceof ConsoleCommandSender)) {
+            Slimefun.getLocalization().sendMessage(sender, "messages.no-permission", true);
+            return;
+        }
+        if (args.length == 3) {
+            Slimefun.getLocalization().sendMessage(sender, "commands.cleardata.confirm", true);
+            return;
+        }
+        if (args.length < 3 || args[3].equalsIgnoreCase("confirm")) {
             Slimefun.getLocalization()
                     .sendMessage(
                             sender,
                             "messages.usage",
                             true,
                             msg -> msg.replace("%usage%", "/sf cleardata <World> <Type>"));
+            return;
+        }
+        List<World> worlds = new ArrayList<>();
+        List<String> clearTypes = new ArrayList<>();
+        String block = Slimefun.getLocalization().getMessage("commands.cleardata.block");
+        String oil = Slimefun.getLocalization().getMessage("commands.cleardata.oil");
+        BlockDataController controller = Slimefun.getDatabaseManager().getBlockDataController();
+        if (args[1].equals("*")) {
+            worlds.addAll(Bukkit.getWorlds());
         } else {
-            Slimefun.getLocalization().sendMessage(sender, "messages.no-permission", true);
+            World toAdd = Bukkit.getWorld(args[1]);
+            if (toAdd == null) {
+                Slimefun.getLocalization().sendMessage(sender, "commands.cleardata.worldNotFound", true);
+                return;
+            }
+        }
+
+        if (args[2].equals("*")) {
+            clearTypes.addAll(ValidClearTypes);
+        } else if (ValidClearTypes.contains(args[2])) {
+            clearTypes.add(args[2]);
+        }
+
+        for (World world : worlds) {
+            for (String cleartype : clearTypes) {
+                if (cleartype.equals("block")) {
+                    controller.removeAllDataInWorldAsync(
+                            world,
+                            () -> Slimefun.runSync(() -> Slimefun.getLocalization()
+                                    .sendMessage(sender, "commands.cleardata.success", true, msg -> msg.replace(
+                                                    "{0}", world.getName())
+                                            .replace("{1}", block))));
+                    continue;
+                }
+                if (cleartype.equals("oil")) {
+                    GEOResource oilresource = null;
+                    for (GEOResource resource :
+                            Slimefun.getRegistry().getGEOResources().values()) {
+                        if (resource.getKey()
+                                .toString()
+                                .equals(new NamespacedKey(Slimefun.instance(), "oil").toString())) {
+                            oilresource = resource;
+                        }
+                    }
+                    controller.removeFromAllChunkInWorldAsync(
+                            world,
+                            oilresource.getKey().toString().replace(":", "-"),
+                            () -> Slimefun.runSync(() -> Slimefun.getLocalization()
+                                    .sendMessage(sender, "commands.cleardata.success", true, msg -> msg.replace(
+                                                    "{0}", world.getName())
+                                            .replace("{1}", oil))));
+                }
+            }
         }
     }
 
