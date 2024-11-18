279eb64191d1201623aeb0dc602683ab1eaa0779
mcchampions <1309635304@qq.com>
2024-11-18 12:42:14 +0800

    Update ChargeUtils.java

--- src/main/java/io/github/thebusybiscuit/slimefun4/utils/ChargeUtils.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/utils/ChargeUtils.java
@@ -28,13 +28,13 @@ import org.bukkit.persistence.PersistentDataType;
  */
 public final class ChargeUtils {
     private static final String LORE_PREFIX = "§8\u21E8 §e\u26A1 §7";
-    private static final String NUMBER_PREFIX = "([+-]?\\d+([.]\\d+)?([Ee][+-]?\\d+)?)";
+    private static final String NUMBER_REGEX = "([+-]?\\d+([.]\\d+)?([Ee][+-]?\\d+)?)";
     private static final Pattern REGEX =
-            Pattern.compile("(§c§o)?"  + LORE_PREFIX + NUMBER_PREFIX + " / " + NUMBER_PREFIX + " J", Pattern.CASE_INSENSITIVE);
+            Pattern.compile("(§c§o)?" + LORE_PREFIX + NUMBER_REGEX + " / " + NUMBER_REGEX + " J", Pattern.CASE_INSENSITIVE);
 
 
     private static final Pattern REGEX_NEW =
-            Pattern.compile(NUMBER_PREFIX + " / " + NUMBER_PREFIX + " J");
+            Pattern.compile(NUMBER_REGEX + " / " + NUMBER_REGEX + " J");
 
     private ChargeUtils() {
     }
