dd48c1af5edeeee52f2063ef15dde566fc2d6389
mcchampions <1309635304@qq.com>
2024-12-13 12:41:01 +0800

    Update MathUtil.java

--- src/main/java/city/norain/slimefun4/utils/MathUtil.java
+++ src/main/java/city/norain/slimefun4/utils/MathUtil.java
@@ -4,7 +4,7 @@ public class MathUtil {
     public static int saturatedAdd(int x, int y) {
         int r = x + y;
         if (((r ^ x) & (r ^ y)) < 0) {
-            return x < 0 ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            return x < 0 ? 0x80000000 : 0x7fffffff;
         } else {
             return r;
         }
