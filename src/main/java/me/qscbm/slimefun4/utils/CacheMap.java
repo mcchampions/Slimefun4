e308b4e3351164a19e0150d75d5f93b667ba85ff
qscbm187531 <1309635304@qq.com>
2025-02-08 19:35:31 +0800

    Update CacheMap.java

--- src/main/java/me/qscbm/slimefun4/utils/CacheMap.java
+++ src/main/java/me/qscbm/slimefun4/utils/CacheMap.java
@@ -2,6 +2,7 @@ package me.qscbm.slimefun4.utils;
 
 import java.util.concurrent.ConcurrentHashMap;
 
+@SuppressWarnings("NullableProblems")
 public class CacheMap extends ConcurrentHashMap<String, String> {
     @Override
     public String put(String key, String value) {
