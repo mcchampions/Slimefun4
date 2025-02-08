51929b2effc2c15004483d8e2c3997234be5c451
qscbm187531 <1309635304@qq.com>
2025-02-09 02:19:11 +0800

    Update Base64Utils.java

--- src/main/java/me/qscbm/slimefun4/utils/Base64Utils.java
+++ src/main/java/me/qscbm/slimefun4/utils/Base64Utils.java
@@ -8,21 +8,21 @@ public class Base64Utils {
     private static final int LINE_SEPARATOR_LENGTH = SYSTEM_LINE_SEPARATOR.length();
 
     public static String encodeLines(byte[] in) {
-        int blockLen = 57;
+        final int blockLen = 57;
 
-        int iLen = in.length;
+        final int iLen = in.length;
 
         int lines = (iLen + 56) / 57;
-        int bufLen = ((iLen + 2) / 3 << 2) + lines * LINE_SEPARATOR_LENGTH;
-        StringBuilder buf = new StringBuilder(bufLen);
+        int builderLen = ((iLen + 2) / 3 << 2) + lines * LINE_SEPARATOR_LENGTH;
+        StringBuilder sb = new StringBuilder(builderLen);
 
         int l;
         for (int ip = 0; ip < iLen; ip += l) {
             l = Math.min(iLen - ip, blockLen);
-            buf.append(Base64Coder.encode(in, ip, l));
-            buf.append(SYSTEM_LINE_SEPARATOR);
+            sb.append(Base64Coder.encode(in, ip, l));
+            sb.append(SYSTEM_LINE_SEPARATOR);
         }
 
-        return buf.toString();
+        return sb.toString();
     }
 }
