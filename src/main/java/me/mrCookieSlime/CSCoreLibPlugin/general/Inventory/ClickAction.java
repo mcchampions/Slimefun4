e22d0e9ceb0f0297883266a7a83e1ce47b668aeb
mcchampions <1309635304@qq.com>
2025-04-27 22:51:50 +0800

    Update ClickAction.java

--- src/main/java/me/mrCookieSlime/CSCoreLibPlugin/general/Inventory/ClickAction.java
+++ src/main/java/me/mrCookieSlime/CSCoreLibPlugin/general/Inventory/ClickAction.java
@@ -1,9 +1,5 @@
 package me.mrCookieSlime.CSCoreLibPlugin.general.Inventory;
 
-import lombok.Getter;
-import org.bukkit.event.inventory.ClickType;
-import org.bukkit.event.inventory.InventoryClickEvent;
-
 /**
  * An old remnant of CS-CoreLib.
  * This will be removed once we updated everything.
@@ -12,19 +8,10 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 public class ClickAction {
     private final boolean right;
     private final boolean shift;
-    @Getter
-    private final boolean numberKey;
 
     public ClickAction(boolean rightClicked, boolean shiftClicked) {
         this.right = rightClicked;
         this.shift = shiftClicked;
-        this.numberKey = false;
-    }
-
-    public ClickAction(InventoryClickEvent e) {
-        this.right = e.isRightClick();
-        this.shift = e.isShiftClick();
-        this.numberKey = e.getClick() == ClickType.NUMBER_KEY;
     }
 
     public boolean isRightClicked() {
