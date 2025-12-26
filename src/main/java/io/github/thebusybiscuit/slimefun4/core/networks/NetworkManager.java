86386238fdc122626c103baaa7ec9c554d23edf3
StarWishsama <noraincity25@gmail.com>
2025-12-27 00:01:02 +0800

    Revert "perf(network): organize network by chunk pos for better relative check"
    This reverts commit a22d344e

--- src/main/java/io/github/thebusybiscuit/slimefun4/core/networks/NetworkManager.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/core/networks/NetworkManager.java
@@ -2,7 +2,6 @@ package io.github.thebusybiscuit.slimefun4.core.networks;
 
 import com.xzavier0722.mc.plugin.slimefun4.storage.util.LocationUtils;
 import io.github.bakedlibs.dough.blocks.BlockPosition;
-import io.github.bakedlibs.dough.blocks.ChunkPosition;
 import io.github.bakedlibs.dough.config.Config;
 import io.github.thebusybiscuit.slimefun4.api.network.Network;
 import io.github.thebusybiscuit.slimefun4.core.debug.Debug;
@@ -12,9 +11,7 @@ import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
 import io.github.thebusybiscuit.slimefun4.implementation.listeners.NetworkListener;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.logging.Level;
 import javax.annotation.Nonnull;
@@ -43,15 +40,12 @@ public class NetworkManager {
     /**
      * Fixes #3041
      *
-     * We use a {@link CopyOnWriteArrayList} to store {@link Network} objects here to ensure thread-safety.
+     * We use a {@link CopyOnWriteArrayList} here to ensure thread-safety.
      * This {@link List} is also much more frequently read than being written to.
      * Therefore a {@link CopyOnWriteArrayList} should be perfect for this, even
      * if insertions come at a slight cost.
-     *
-     * For large-scaled networks, we use {@link ChunkPosition} as key
-     * for quicker access to relevant networks in certain chunks.
      */
-    private final Map<ChunkPosition, List<Network>> networks = new ConcurrentHashMap<>();
+    private final List<Network> networks = new CopyOnWriteArrayList<>();
 
     /**
      * This creates a new {@link NetworkManager} with the given capacity.
@@ -118,7 +112,7 @@ public class NetworkManager {
      */
     @Nonnull
     public List<Network> getNetworkList() {
-        return networks.values().stream().flatMap(List::stream).toList();
+        return Collections.unmodifiableList(networks);
     }
 
     @Nonnull
@@ -129,14 +123,7 @@ public class NetworkManager {
 
         Validate.notNull(type, "Type must not be null");
 
-        ChunkPosition chunkPos = new ChunkPosition(l);
-        List<Network> chunkNetworks = networks.get(chunkPos);
-
-        if (chunkNetworks == null) {
-            return Optional.empty();
-        }
-
-        for (Network network : chunkNetworks) {
+        for (Network network : networks) {
             if (type.isInstance(network) && network.connectsTo(l)) {
                 return Optional.of(type.cast(network));
             }
@@ -155,14 +142,7 @@ public class NetworkManager {
         Validate.notNull(type, "Type must not be null");
         List<T> list = new ArrayList<>();
 
-        ChunkPosition chunkPos = new ChunkPosition(l);
-        List<Network> chunkNetworks = networks.get(chunkPos);
-
-        if (chunkNetworks == null) {
-            return list;
-        }
-
-        for (Network network : chunkNetworks) {
+        for (Network network : networks) {
             if (type.isInstance(network) && network.connectsTo(l)) {
                 list.add(type.cast(network));
             }
@@ -183,15 +163,7 @@ public class NetworkManager {
         Debug.log(
                 TestCase.ENERGYNET, "Registering network @ " + LocationUtils.locationToString(network.getRegulator()));
 
-        var chunkPos = new ChunkPosition(network.getRegulator());
-
-        networks.compute(chunkPos, (pos, nets) -> {
-            if (nets == null) {
-                nets = new CopyOnWriteArrayList<>();
-            }
-            nets.add(network);
-            return nets;
-        });
+        networks.add(network);
     }
 
     /**
@@ -207,12 +179,7 @@ public class NetworkManager {
                 TestCase.ENERGYNET,
                 "Unregistering network @ " + LocationUtils.locationToString(network.getRegulator()));
 
-        ChunkPosition chunkPos = new ChunkPosition(network.getRegulator());
-
-        networks.computeIfPresent(chunkPos, (pos, nets) -> {
-            nets.remove(network);
-            return nets.isEmpty() ? null : nets;
-        });
+        networks.remove(network);
     }
 
     /**
