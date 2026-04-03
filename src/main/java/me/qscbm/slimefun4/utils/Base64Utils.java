7c755eca7bf4d3c2e2204b896b6ff0c19c48a311
mcchampions <1309635304@qq.com>
2026-04-04 00:58:38 +0800

    Update Base64Utils.java

--- src/main/java/me/qscbm/slimefun4/utils/Base64Utils.java
+++ src/main/java/me/qscbm/slimefun4/utils/Base64Utils.java
@@ -1,6 +1,5 @@
 package me.qscbm.slimefun4.utils;
 
-import org.yaml.snakeyaml.external.biz.base64Coder.Base64Coder;
 
 public class Base64Utils {
     private static final String SYSTEM_LINE_SEPARATOR = System.lineSeparator();
@@ -19,10 +18,62 @@ public class Base64Utils {
         int l;
         for (int ip = 0; ip < iLen; ip += l) {
             l = Math.min(iLen - ip, blockLen);
-            sb.append(Base64Coder.encode(in, ip, l));
+            sb.append(encode(in, ip, l));
             sb.append(SYSTEM_LINE_SEPARATOR);
         }
 
         return sb.toString();
+    }
+    private static final char[] map1 = new char[64];
+    public static char[] encode(byte[] in, int iOff, int iLen) {
+        int oDataLen = (iLen * 4 + 2) / 3;
+        int oLen = (iLen + 2) / 3 * 4;
+        char[] out = new char[oLen];
+        int ip = iOff;
+        int iEnd = iOff + iLen;
+
+        for(int op = 0; ip < iEnd; ++op) {
+            int i0 = in[ip++] & 255;
+            int i1 = ip < iEnd ? in[ip++] & 255 : 0;
+            int i2 = ip < iEnd ? in[ip++] & 255 : 0;
+            int o0 = i0 >>> 2;
+            int o1 = (i0 & 3) << 4 | i1 >>> 4;
+            int o2 = (i1 & 15) << 2 | i2 >>> 6;
+            int o3 = i2 & 63;
+            out[op++] = map1[o0];
+            out[op++] = map1[o1];
+            out[op] = op < oDataLen ? map1[o2] : 61;
+            ++op;
+            out[op] = op < oDataLen ? map1[o3] : 61;
+        }
+
+        return out;
+    }
+    private static final byte[] map2;
+    static {
+        int i = 0;
+
+        char c;
+        for(c = 'A'; c <= 'Z'; map1[i++] = c++) {
+        }
+
+        for(c = 'a'; c <= 'z'; map1[i++] = c++) {
+        }
+
+        for(c = '0'; c <= '9'; map1[i++] = c++) {
+        }
+
+        map1[i++] = '+';
+        map1[i++] = '/';
+        map2 = new byte[128];
+
+        for(i = 0; i < map2.length; ++i) {
+            map2[i] = -1;
+        }
+
+        for(i = 0; i < 64; ++i) {
+            map2[map1[i]] = (byte)i;
+        }
+
     }
 }
