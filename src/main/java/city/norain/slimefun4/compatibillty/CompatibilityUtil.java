7048c20026349b9ff2a256fd62b08d06e3b2891c
mcchampions <1309635304@qq.com>
2026-04-04 12:42:32 +0800

    perf

--- src/main/java/city/norain/slimefun4/compatibillty/CompatibilityUtil.java
+++ src/main/java/city/norain/slimefun4/compatibillty/CompatibilityUtil.java
@@ -23,38 +23,7 @@ public class CompatibilityUtil {
      * @return 放置此方块所使用的材质
      */
     public Material getPlacementMaterial(BlockData blockData) {
-        if (SlimefunExtended.getMinecraftVersion().isAtLeast(1, 19, 4)) {
-            return blockData.getPlacementMaterial();
-        } else {
-            switch (blockData.getMaterial()) {
-                case PLAYER_WALL_HEAD -> {
-                    return Material.PLAYER_HEAD;
-                }
-                case REDSTONE_WIRE -> {
-                    return Material.REDSTONE;
-                }
-                default -> {
-                    var mat = blockData.getMaterial();
-                    var enumName = blockData.getMaterial().name();
-
-                    if (Ageable.class.equals(mat.data) && enumName.endsWith("S")) {
-                        var itemMat = Material.getMaterial(enumName.substring(0, enumName.length() - 1));
-                        return itemMat != null && itemMat.isItem() ? itemMat : mat;
-                    }
-
-                    if (WallSign.class.equals(mat.data) && enumName.contains("_WALL_")) {
-                        Material itemMat = Material.getMaterial(enumName.replace("_WALL_", "_"));
-
-                        if (itemMat != null && itemMat.isItem()) {
-                            return mat;
-                        }
-                    }
-
-                    // Fallback to original material
-                    return blockData.getMaterial();
-                }
-            }
-        }
+        return blockData.getPlacementMaterial();
     }
 
     /**
@@ -65,8 +34,7 @@ public class CompatibilityUtil {
      * @return 玩家连接或在线
      */
     public boolean isConnected(OfflinePlayer player) {
-        if (SlimefunExtended.getMinecraftVersion().isAtLeast(1, 20)
-                && Slimefun.instance().getServer().getOnlineMode()) {
+        if (Slimefun.instance().getServer().getOnlineMode()) {
             return player.isConnected();
         } else {
             return player.isOnline();
@@ -80,10 +48,6 @@ public class CompatibilityUtil {
      * @return 最大生命值属性
      */
     public static Attribute getMaxHealth() {
-        if (SlimefunExtended.getMinecraftVersion().isAtLeast(1, 21, 3)) {
-            return Registry.ATTRIBUTE.get(NamespacedKey.fromString("max_health"));
-        } else {
-            return Attribute.valueOf("GENERIC_MAX_HEALTH");
-        }
+        return Registry.ATTRIBUTE.get(NamespacedKey.fromString("max_health"));
     }
 }
--- src/main/java/city/norain/slimefun4/compatibillty/VersionedEvent.java
+++ src/main/java/city/norain/slimefun4/compatibillty/VersionedEvent.java
@@ -1,11 +1,7 @@
 package city.norain.slimefun4.compatibillty;
 
-import io.github.thebusybiscuit.slimefun4.api.MinecraftVersion;
-import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Method;
 import java.util.List;
-import java.util.logging.Level;
+
 import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
 import org.bukkit.ExplosionResult;
@@ -17,41 +13,12 @@ import org.bukkit.inventory.Inventory;
 
 @UtilityClass
 public class VersionedEvent {
-    private Constructor<BlockExplodeEvent> BLOCK_EXPLODE_EVENT_CONSTRUCTOR;
-
-    private Method GET_TOP_INVENTORY;
-    private Method GET_CLICKED_INVENTORY;
-
     public void init() {
-        if (Slimefun.getMinecraftVersion().isBefore(MinecraftVersion.MINECRAFT_1_21)) {
-            try {
-                BLOCK_EXPLODE_EVENT_CONSTRUCTOR =
-                        BlockExplodeEvent.class.getConstructor(Block.class, List.class, float.class);
-
-                GET_TOP_INVENTORY =
-                        Class.forName("org.bukkit.inventory.InventoryView").getMethod("getTopInventory");
-                GET_TOP_INVENTORY.setAccessible(true);
-
-                GET_CLICKED_INVENTORY = Class.forName("org.bukkit.event.inventory.InventoryClickEvent")
-                        .getMethod("getClickedInventory");
-                GET_CLICKED_INVENTORY.setAccessible(true);
-
-            } catch (NoSuchMethodException | ClassNotFoundException e) {
-                Slimefun.logger().log(Level.WARNING, "无法初始化事件版本兼容模块, 部分功能可能无法正常使用", e);
-            }
-        }
     }
 
     @SneakyThrows
     public BlockExplodeEvent newBlockExplodeEvent(Block block, List<Block> affectedBlock, float yield) {
-        if (Slimefun.getMinecraftVersion().isAtLeast(MinecraftVersion.MINECRAFT_1_21)) {
-            return new BlockExplodeEvent(block, block.getState(), affectedBlock, yield, ExplosionResult.DESTROY);
-        } else {
-            if (BLOCK_EXPLODE_EVENT_CONSTRUCTOR == null) {
-                throw new IllegalStateException("Unable to create BlockExplodeEvent: missing constructor");
-            }
-            return BLOCK_EXPLODE_EVENT_CONSTRUCTOR.newInstance(block, affectedBlock, yield);
-        }
+        return new BlockExplodeEvent(block, block.getState(), affectedBlock, yield, ExplosionResult.DESTROY);
     }
 
     /**
@@ -59,27 +26,11 @@ public class VersionedEvent {
      */
     @SneakyThrows
     public Inventory getTopInventory(InventoryEvent event) {
-        if (Slimefun.getMinecraftVersion().isAtLeast(MinecraftVersion.MINECRAFT_1_21)) {
-            return event.getView().getTopInventory();
-        } else {
-            if (GET_TOP_INVENTORY == null) {
-                throw new IllegalStateException("Unable to get top inventory: missing method");
-            }
-
-            return (Inventory) GET_TOP_INVENTORY.invoke(event.getView());
-        }
+        return event.getView().getTopInventory();
     }
 
     @SneakyThrows
     public Inventory getClickedInventory(InventoryClickEvent event) {
-        if (Slimefun.getMinecraftVersion().isAtLeast(MinecraftVersion.MINECRAFT_1_21)) {
-            return event.getClickedInventory();
-        } else {
-            if (GET_CLICKED_INVENTORY == null) {
-                throw new IllegalStateException("Unable to get clicked inventory: missing method");
-            }
-
-            return (Inventory) GET_CLICKED_INVENTORY.invoke(event);
-        }
+        return event.getClickedInventory();
     }
 }
