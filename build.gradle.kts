ffcaf8f07b55c5e80f3df88241631f7761972ae2
StarWishsama <noraincity25@gmail.com>
2026-04-20 23:09:25 +0800

    refactor(gradle): move repositories to build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,19 @@ tasks.compileJava {
     options.release.set(21)
 }
 
+repositories {
+    mavenCentral()
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots")
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://maven.norain.city/snapshots")
+    maven("https://jitpack.io")
+    maven("https://maven.enginehub.org/repo/")
+    maven("https://repo.extendedclip.com/content/repositories/placeholderapi")
+    maven("https://nexus.neetgames.com/repository/maven-public")
+    maven("https://repo.walshy.dev/public")
+    maven("https://repo.codemc.io/repository/maven-public/")
+}
+
 dependencies {
     compileOnly(libs.paper.api)
     compileOnly(libs.jsr305)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,1 @@
 rootProject.name = "Slimefun"
-
-dependencyResolutionManagement {
-    repositories {
-        mavenCentral()
-        maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots")
-        maven("https://repo.papermc.io/repository/maven-public/")
-        maven("https://maven.norain.city/snapshots")
-        maven("https://jitpack.io")
-        maven("https://maven.enginehub.org/repo/")
-        maven("https://repo.extendedclip.com/content/repositories/placeholderapi")
-        maven("https://nexus.neetgames.com/repository/maven-public")
-        maven("https://repo.walshy.dev/public")
-        maven("https://repo.codemc.io/repository/maven-public/")
-    }
-}
